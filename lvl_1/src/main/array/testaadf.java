df947b3e12536c559c2e20187ee3c5929966fad3
dhlee <bfh3000@naver.com>
2022-06-06 18:28:43 +0900

    update workspace 20220606

--- lvl_1/src/main/array/testaadf.java
+++
@@ -1,9 +0,0 @@
-public class testaadf {
-    public static void main(String[] args) {
-        System.out.println("hsdasdf");
-        System.out.println("hsdasdf");
-        System.out.println("hsdasdf");
-        System.out.println("hsdasdf");
-        System.out.println("hsdasdf");
-    }
-}
