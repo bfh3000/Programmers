43abcbf857b7769267883259e69bbee0dcb60ce5
bfh3000 <bfh3000@naver.com>
2022-06-12 14:46:06 +0900

    git ignore update

--- lvl_1/src/main/array/n20220606.java
+++ lvl_1/src/main/array/n20220606.java
@@ -3,6 +3,7 @@
 // 알게된 점
 // 1. Math.pow()로 radix를 10진수로 변경하는 함수가 있다는 사실.
 // 2. 10진수는 Integer로 radix로 변경이 가능하다는 사실. (하지만, 그 역은 성립되지 않음.)
+// 3. 음.. 1위 솔루션 보니깐 "2."의 역은 성립이 되네. Integer Int로 되네 ( String to int로 )
 
 
 import org.apache.commons.logging.Log;
@@ -13,16 +14,13 @@ public class n20220606 {
     private static Log log = LogFactory.getLog(n20220606.class);
 
     public static void main(String[] args) {
-//        Solution t = new Solution();
-//        System.out.println(t.solution(45));
-        
-        Solution t2 = new Solution();
-        System.out.println("result value : "+ t2.solution2(45));
+        Solution t = new Solution();
+        System.out.println("result value : " + t.solution1(45));
     }
 
 
     static class mySolution {
-
+        
     }
 
 
@@ -30,30 +28,31 @@ public class n20220606 {
         public int solution2(int inputInt) {
             int answer = 0;
             String third = Integer.toString(inputInt, 3);
-            System.out.println("시작"+third);
+            System.out.println("시작" + third);
 
             StringBuffer sb = new StringBuffer(third);
-            System.out.println("SB : "+sb);
+            System.out.println("SB : " + sb);
 
             String reversed = sb.reverse().toString();
-            System.out.println("reversed : "+reversed);
+            System.out.println("reversed : " + reversed);
 
             int exp = 0;
             System.out.println("============================================================================s");
             for (int i = reversed.length() - 1; i >= 0; i--) {
                 System.out.println("");
-                System.out.println(i+"번째");
+                System.out.println(i + "번째");
+
                 int tmp = (int) (Integer.parseInt(String.valueOf(reversed.charAt(i))) * Math.pow(3, exp));
-                System.out.println("tmp : "+tmp);
+                System.out.println("tmp : " + tmp);
                 answer += tmp;
-                System.out.println("answer : "+answer);
+                System.out.println("answer : " + answer);
                 exp++;
             }
 
             return answer;
         }
 
-        public int solution(int n) {
+        public int solution1(int n) {
             String a = "";
 
             while (n > 0) {
@@ -61,10 +60,9 @@ public class n20220606 {
                 n /= 3;
             }
             a = new StringBuilder(a).reverse().toString();
-            return Integer.parseInt(a, 3);
+            int result = Integer.parseInt(a, 3);
+            return result;
         }
-
-
     }
 }
 
