249259f59b95f48f9d9525da7ca72c93f4a97e62
bfh3000 <bfh3000@naver.com>
2022-07-13 14:18:45 +0900

    update 20220612

--- practice/src/main/java/my/example/SocketTest.java
+++ practice/src/main/java/my/example/SocketTest.java
@@ -2,11 +2,14 @@ package my.example;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketTest {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(6000)) {
-            
+            System.out.println("Wating for connections.......");
+            Socket socket = serverSocket.accept();
+            System.out.println("Connected to client");
         }catch (IOException e){
             e.printStackTrace();
         }
